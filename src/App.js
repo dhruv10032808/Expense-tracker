0609ad94a1c5650a5ded5a0c6c09d22735fdebc4
dhruv10032808 <dhruv.bhanu.1003@gmail.com>
2023-02-08 19:21:35 +0530

    login

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import AuthForm from './components/AuthForm';
+import { Route,Routes } from 'react-router-dom';
+import AuthForm from './components/Auth/AuthForm';
+import HomePage from './components/pages/HomePage';
+
 
 function App() {
-  return <AuthForm/>
+  return <>
+  <Routes>
+  <Route path='/' exact element={<AuthForm />}></Route>
+  <Route path='/home' exact element={<HomePage/>}></Route>
+  </Routes>
+  </>
 }
 
 export default App;
---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,5 @@
+import React from "react";
+const HomePage=()=>{
+return(<div><h1>Welcome to Expense Tracker</h1></div>)
+}
+export default HomePage;
